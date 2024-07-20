16958cc04a51002a698c797b2c072a1c7c56fcae
udevtools <anthony.cardinale06@gmail.com>
2024-07-20 10:16:47 +0200

    Update SafeString.cs

--- Runtime/SafeString.cs
+++ Runtime/SafeString.cs
@@ -15,7 +15,7 @@ public class SafeString
     public string Value { 
         get 
         { 
-            if (protectedValue.CompareValue(Value))
+            if (protectedValue.CompareValue(_value))
             {
                 return _value;
             }
@@ -28,7 +28,7 @@ public class SafeString
         }
         set
         {
-            if (protectedValue.CompareValue(Value))
+            if (protectedValue.CompareValue(_value))
             {
                 _value = value;
                 protectedValue.ApplyNewValue(_value);
